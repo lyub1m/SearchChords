9e4fda07e4ecf661ba69027bcd7a5437d6698e4a
lyub1m <danil.lyubimov.2002@gmail.com>
2022-07-20 21:40:31 +0300

    Added: divs, css

---
+++ client/src/App.js
@@ -0,0 +1,29 @@
+
+import './App.css';
+
+function App() {
+  return (
+    <div className="wrapper">
+      <div className="container">
+        <div className="header">
+
+        </div>
+        <div className="main-container">
+          <div className="left-sidebar">
+
+          </div>
+          <div className="content">
+
+          </div>
+          <div className="right-sidebar">
+
+          </div>
+        </div>
+
+      </div>
+
+    </div>
+  );
+}
+
+export default App;
